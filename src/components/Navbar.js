364c608cb9f7ca81e1104a35c31f504a1336484a
RobsonTrasel <robson.trasel@gmail.com>
2022-10-20 02:30:04 -0300

    feat: navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,37 @@
-import '../assets/styles/Navbar.css'
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../assets/styles/Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="header">
+      <Link to="/">Robson Trasel</Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projetos</Link>
+        </li>
+        <li>
+          <Link to="/about">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contato</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#FFF" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#FFF" }} />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
