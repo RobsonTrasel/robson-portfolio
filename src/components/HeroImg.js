535c975582ab6144ce0dbd6b713d157d4dc1c510
RobsonTrasel <robson.trasel@gmail.com>
2022-10-20 02:58:04 -0300

    chore: navbar and home page almost done!

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,9 +1,23 @@
-import '../assets/styles/HeroImg.css'
+import "../assets/styles/HeroImg.css";
+import Bg from '../assets/imgs/bg.jpg'
+import {Link} from 'react-router-dom'
 
 const HeroImg = () => {
   return (
-    <div>HeroImg</div>
-  )
-}
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={Bg} alt="" />
+      </div>
+      <div className="content">
+        <p>Olá, sou Robson</p>
+        <h1>Desenvolvedor Fullstack</h1>
+        <div>
+          <Link className="btn" to='/projects'>Projetos</Link>
+          <Link className="btn btn-light" to='/contact'>Contato</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HeroImg  
+export default HeroImg;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
+import HeroImg from "../components/HeroImg";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div><Navbar/></div>
+  return (
+    <div>
+      <Navbar/>
+      <HeroImg/>
+    </div>
+  )
+  
 };
